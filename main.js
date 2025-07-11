4f1a726aaa05dabcfd0b8530225b025ae7e47deb
echoq9019 <echoq9019@gmail.com>
2025-07-10 23:36:37 -0300

    Para Ela
    Para Ela

---
+++ main.js
@@ -0,0 +1,4 @@
+// Faz o formulário aparecer se clicar no lugar certo
+document.getElementById('terminal').addEventListener('click', () => {
+    document.getElementById('hidden-form').style.display = 'block';
+});
