e63cb10d7305feda994d71bf7e5fb7eaf70e30ca
echoq9019 <echoq9019@gmail.com>
2025-07-10 23:55:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-// Faz o formulário aparecer se clicar no lugar certo
-document.getElementById('terminal').addEventListener('click', () => {
-    document.getElementById('hidden-form').style.display = 'block';
+document.getElementById('hidden-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const username = this.username.value.toLowerCase().trim();
+  const password = this.password.value.trim();
+
+  const validUser = 'elara';
+  const validPass = 'quinn2146';
+
+  if (username === validUser && password === validPass) {
+    alert('Login autorizado. Bem-vinda, ' + username + '!');
+    window.location.href = 'segredo.html';
+  } else {
+    alert('Nome ou senha incorretos. Tente novamente.');
+  }
 });
