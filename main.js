c8a8337a3122a450b24a0cb31d56f6eecce80f48
echoq9019 <echoq9019@gmail.com>
2025-07-11 00:16:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+document.getElementById('clickable').addEventListener('click', () => {
+  document.getElementById('loginFormContainer').style.display = 'block';
+});
+
 document.getElementById('loginForm').addEventListener('submit', function(e) {
   e.preventDefault();
 
@@ -5,7 +9,7 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
   const password = document.getElementById('password').value.trim().toLowerCase();
 
   if (username === 'elara' && password === 'quinn2146') {
-    window.location.href = 'secret.html';
+    window.location.href = './secret.html';
   } else {
     alert('Incorrect. Try again.');
   }
